90aa3892fd499743a89aa49dbca03e48e00a1428
jarontai <jaroncn@gmail.com>
2019-05-29 15:07:25 +0800

    Init with stagehand

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dash/dash.dart' as dash;
+
+main(List<String> arguments) {
+  print('Hello world: ${dash.calculate()}!');
+}
---
+++ test/dash_test.dart
@@ -0,0 +1,8 @@
+import 'package:dash/dash.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
