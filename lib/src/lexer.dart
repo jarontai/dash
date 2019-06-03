528db6552ef98fd3ccbeeadcea3f2ea33e9555c8
jarontai <jaroncn@gmail.com>
2019-06-03 22:10:34 +0800

    Prefer constant

--- lib/src/lexer.dart
+++ lib/src/lexer.dart
@@ -151,19 +151,16 @@ class Lexer with CharHelper {
   }
 }
 
+/// A object represents the basic unit of lexical analysis.
 class Token {
-  TokenType tokenType;
-  String literal;
+  final TokenType tokenType;
+  final String literal;
 
-  Token(this.tokenType, this.literal);
+  const Token(this.tokenType, this.literal);
 
-  Token.identifier(this.literal) {
-    tokenType = TokenType.IDENTIFIER;
-  }
+  const Token.identifier(this.literal) : tokenType = TokenType.IDENTIFIER;
 
-  Token.number(this.literal) {
-    tokenType = TokenType.NUMBER;
-  }
+  const Token.number(this.literal) : tokenType = TokenType.NUMBER;
 
   String toString() {
     return 'Token(type: $tokenType, literal: $literal)';
@@ -207,34 +204,35 @@ enum TokenType {
   FALSE,
 }
 
+/// The pre-defined [Token] constants.
 abstract class Tokens {
-  static final Token illegal = Token(TokenType.ILLEGAL, 'ILLEGAL');
-  static final Token eof = Token(TokenType.EOF, '');
+  static const Token illegal = const Token(TokenType.ILLEGAL, 'ILLEGAL');
+  static const Token eof = const Token(TokenType.EOF, '');
 
-  static final Token assign = Token(TokenType.ASSIGN, '=');
-  static final Token plus = Token(TokenType.PLUS, '+');
-  static final Token minus = Token(TokenType.MINUS, '-');
-  static final Token mul = Token(TokenType.MUL, '*');
-  static final Token div = Token(TokenType.DIV, '/');
-  static final Token bang = Token(TokenType.BANG, '!');
-  static final Token gt = Token(TokenType.GT, '>');
-  static final Token gte = Token(TokenType.GTE, '>=');
-  static final Token lt = Token(TokenType.LT, '<');
-  static final Token lte = Token(TokenType.LTE, '<=');
-  static final Token eq = Token(TokenType.EQ, '==');
-  static final Token neq = Token(TokenType.NEQ, '!=');
+  static const Token assign = const Token(TokenType.ASSIGN, '=');
+  static const Token plus = const Token(TokenType.PLUS, '+');
+  static const Token minus = const Token(TokenType.MINUS, '-');
+  static const Token mul = const Token(TokenType.MUL, '*');
+  static const Token div = const Token(TokenType.DIV, '/');
+  static const Token bang = const Token(TokenType.BANG, '!');
+  static const Token gt = const Token(TokenType.GT, '>');
+  static const Token gte = const Token(TokenType.GTE, '>=');
+  static const Token lt = const Token(TokenType.LT, '<');
+  static const Token lte = const Token(TokenType.LTE, '<=');
+  static const Token eq = const Token(TokenType.EQ, '==');
+  static const Token neq = const Token(TokenType.NEQ, '!=');
 
-  static final Token comma = Token(TokenType.COMMA, ',');
-  static final Token semi = Token(TokenType.SEMI, ';');
-  static final Token lparen = Token(TokenType.LPAREN, '(');
-  static final Token rparen = Token(TokenType.RPAREN, ')');
-  static final Token lbrace = Token(TokenType.LBRACE, '{');
-  static final Token rbrace = Token(TokenType.RBRACE, '}');
+  static const Token comma = const Token(TokenType.COMMA, ',');
+  static const Token semi = const Token(TokenType.SEMI, ';');
+  static const Token lparen = const Token(TokenType.LPAREN, '(');
+  static const Token rparen = const Token(TokenType.RPAREN, ')');
+  static const Token lbrace = const Token(TokenType.LBRACE, '{');
+  static const Token rbrace = const Token(TokenType.RBRACE, '}');
 
-  static final Token kVar = Token(TokenType.VAR, 'var');
-  static final Token kReturn = Token(TokenType.RETURN, 'return');
-  static final Token kIf = Token(TokenType.IF, 'if');
-  static final Token kElse = Token(TokenType.ELSE, 'else');
-  static final Token kTrue = Token(TokenType.TRUE, 'true');
-  static final Token kFalse = Token(TokenType.FALSE, 'false');
+  static const Token kVar = const Token(TokenType.VAR, 'var');
+  static const Token kReturn = const Token(TokenType.RETURN, 'return');
+  static const Token kIf = const Token(TokenType.IF, 'if');
+  static const Token kElse = const Token(TokenType.ELSE, 'else');
+  static const Token kTrue = const Token(TokenType.TRUE, 'true');
+  static const Token kFalse = const Token(TokenType.FALSE, 'false');
 }
